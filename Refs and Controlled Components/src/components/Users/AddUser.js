d8b815192699f00971a4e6fc9fde6c2fe20e5d9b
sumitraee0079 <31108006+sumitraee0079@users.noreply.github.com>
2023-03-29 02:37:58 +0530

    Create AddUser.js

---
+++ Refs and Controlled Components/src/components/Users/AddUser.js
@@ -0,0 +1,70 @@
+import React, { useState, useRef } from 'react';
+
+import Card from '../UI/Card';
+import Button from '../UI/Button';
+import ErrorModal from '../UI/ErrorModal';
+import Wrapper from '../Helpers/Wrapper';
+import classes from './AddUser.module.css';
+
+const AddUser = (props) => {
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+  const collegenameInputRef = useRef();
+  
+  const [error, setError] = useState();
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    const enteredName = nameInputRef.current.value;
+    const enteredUserAge = ageInputRef.current.value;
+    const enteredCollegename = collegenameInputRef.current.value;
+    
+    if (enteredName.trim().length === 0 || enteredUserAge.trim().length === 0 || enteredCollegename.trim().length === 0) {
+      setError({
+        title: 'Invalid input',
+        message: 'Please enter a valid name, age and collegename(non-empty values).',
+      });
+      return;
+    }
+    if (+enteredUserAge < 1) {
+      setError({
+        title: 'Invalid age',
+        message: 'Please enter a valid age (> 0).',
+      });
+      return;
+    }
+    props.onAddUser(enteredName, enteredUserAge, collegename);
+    nameInputRef.current.value = '';
+    ageInputRef.current.value = '';
+    collegenameInputRef.current.value = '';
+  };
+
+  const errorHandler = () => {
+    setError(null);
+  };
+
+  return (
+    <Wrapper>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          <input id="username" type="text" ref={nameInputRef} />
+          <label htmlFor="age">Age (Years)</label>
+          <input id="age" type="number" ref={ageInputRef} />
+            <label htmlFor="age">College Name</label>
+          <input id="clgname" type="text" ref={collegenameInputRef} />
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </Wrapper>
+  );
+};
+
+export default AddUser;
